e04f42add66ae28d9c4efba1c4cedf2ca94b513b
Colin Angus <c.r.angus@sheffield.ac.uk>
2022-12-02 11:16:07 +0000

    Update ConstituencyTernaryElections.R

--- ConstituencyTernaryElections.R
+++ ConstituencyTernaryElections.R
@@ -1,9 +1,9 @@
 rm(list=ls())
 
 #ggtern only works with older version of ggplot
-require(devtools)
-install_version("ggplot2", version = "3.3.2", repos = "http://cran.us.r-project.org")
-install_version("ggtern", version = "3.3.0", repos = "http://cran.us.r-project.org")
+#require(devtools)
+#install_version("ggplot2", version = "3.3.2", repos = "http://cran.us.r-project.org")
+#install_version("ggtern", version = "3.3.0", repos = "http://cran.us.r-project.org")
 
 library(dplyr)
 library(ggplot2)
@@ -18,25 +18,26 @@ library(cowplot)
 
 #Read in historic election data from House of Commons library
 temp <- tempfile()
-source <- "https://researchbriefings.files.parliament.uk/documents/CBP-8647/1918_2019election_results.csv"
+source <- "https://researchbriefings.files.parliament.uk/documents/CBP-8647/1918-2019election_results.csv"
 temp <- curl_download(url=source, destfile=temp, quiet=FALSE, mode="wb")
 
 data <- read.csv(temp) %>% 
   #Keep only English data as other parties are too large a factor elsewhere
   filter(!country.region %in% c("Scotland", "Wales", "Northern Ireland")) %>% 
   #Tidy up the constituency names
-  mutate(constituency=to_upper_camel_case(gsub("&", "and", constituency), sep_out=" "),
+  mutate(constituency_name=to_upper_camel_case(gsub("&", "and", constituency_name), sep_out=" "),
          lab_votes=if_else(is.na(lab_votes), 0, as.double(lab_votes)),
          lib_votes=if_else(is.na(lib_votes), 0, as.double(lib_votes)),
          con_votes=if_else(is.na(con_votes), 0, as.double(con_votes))) %>% 
   #Add in post-2019 by-election results manually
-  bind_rows(data.frame(constituency=c("Hartlepool", "Chesham And Amersham", 
+  bind_rows(data.frame(constituency_name=c("Hartlepool", "Chesham And Amersham", 
                                       "Batley And Spen", "North Shropshire",
-                                      "Tiverton And Honiton", "Wakefield"),
-            lab_votes=c(8589, 622, 13296, 3686, 1562, 13166),
-            lib_votes=c(349, 21517, 1254, 17957, 22537, 508),
-            con_votes=c(15529, 13489, 12973, 12032, 16393, 8241),
-            election=c("2021", "2021", "2021", "2021", "2022", "2022"))) %>% 
+                                      "Tiverton And Honiton", "Wakefield",
+                                      "City Of Chester"),
+            lab_votes=c(8589, 622, 13296, 3686, 1562, 13166, 17309),
+            lib_votes=c(349, 21517, 1254, 17957, 22537, 508, 2368),
+            con_votes=c(15529, 13489, 12973, 12032, 16393, 8241, 6335),
+            election=c("2021", "2021", "2021", "2021", "2022", "2022", "2022"))) %>% 
   rowwise() %>% 
   #Calculate % of votes going to main parties which went to each one
   mutate(Lab_prop=lab_votes/(lab_votes+con_votes+lib_votes),
@@ -51,9 +52,9 @@ data <- read.csv(temp) %>%
            election=="1974F" ~ 1974,
            election=="1974O" ~ 1974.5,
            TRUE ~ as.numeric(election))) %>% 
-  select(constituency, Lab_prop, Lib_prop, Con_prop, election, winner) %>% 
+  select(constituency_name, Lab_prop, Lib_prop, Con_prop, election, winner) %>% 
   ungroup() %>% 
-  group_by(constituency) %>% 
+  group_by(constituency_name) %>% 
   arrange(-election) %>% 
   ungroup()
 
@@ -71,13 +72,13 @@ lab <- data.frame(z=c(0,0.5,1/3, 0),
 
 background <- rbind(con, lib, lab)
 
-const <- "Wakefield"
+const <- "City Of Chester"
 
-plotdata <- data %>% filter(constituency==const & election>=1974)
+plotdata <- data %>% filter(constituency_name==const & election>=1974)
 plottitle <- paste("Electoral shifts in", const, "since", min(plotdata$election))
 
 
-agg_tiff("Outputs/UKElectionsWakefield.tiff", units="in", width=6, height=6, res=800)
+agg_tiff("Outputs/UKElectionsChester.tiff", units="in", width=6, height=6, res=800)
 ggtern()+
   geom_polygon(data=background, aes(x, y, z,fill=Col), alpha=0.2)+
   theme_hidegrid()+
@@ -202,9 +203,6 @@ plot2 <- ggplot()+
 agg_png("Outputs/UKElectionsTernaryCartogram2022.png", units="in", width=9, height=10, res=800)
 plot2
 dev.off()
-
-
-
 
 
 
